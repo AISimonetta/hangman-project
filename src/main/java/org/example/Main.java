8be4d4939bc1a17ef1944f391a4fea09b9d0d4f4
AISimonetta <info@simonettaalejandra.com>
2024-01-25 11:42:29 +0000

    isWordGuessed,displayGameStatus & checkGuess completed. New method added= updateGuessedWord

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class Main {
+
+    //*****VARIABLES
     private String wordToGuess;
     private String[] guessedWord;
     private String guessedLetters = "";
@@ -9,6 +11,7 @@ public class Main {
     private ResultsDisplay resultDisplay;
     private UserInteraction userInteraction;
 
+    //*****CONSTRUCTORS
     public Main(String wordToGuess, String[] guessedWord, String guessedLetters, int lives) {
         this.wordToGuess = wordToGuess;
         this.guessedWord = guessedWord;
@@ -20,6 +23,7 @@ public class Main {
         startGame(wordGuesses);
     }
 
+    //******METHODS
     //method to start game
     private void startGame(String[] wordGuesses) {
         randomWords = new RandomWords(wordGuesses);
@@ -28,7 +32,7 @@ public class Main {
         wordToGuess = randomWords.getRandomWord();
         guessedWord = new String[wordToGuess.length()];
         lives = 10;
-        //start word with __
+        //start words with __
         for (int i = 0; i < guessedWord.length; i++) {
             guessedWord[i] = "_";
         }
@@ -39,27 +43,51 @@ public class Main {
         while (lives > 0 && !isWordGuessed()) {
             displayGameStatus();
             char guess = userInteraction.getGuessCharacter();
-            checkGuess(guess); // error with or without char..ask charlie
+            checkGuess(guess);
         }
         displayGameResult();
     }
 
     //to check if word is guessed by the _ left
     private boolean isWordGuessed() {
-        return false;
+        for (String letter : guessedWord) {
+            if (letter.equals("_")) {
+                return false;
+            }
+        }
+        return true;
     }
-
 
     //show status of my user interaction methods :displayWord,displayGuessedLetters & displayLives
     private void displayGameStatus() {
+        resultDisplay.displayGuessedLetters(guessedLetters);
+        resultDisplay.displayWord(guessedWord);
+        resultDisplay.displayLives(lives);
     }
 
     // keep track guesses and lives
+    //IntelliJ recommended wrapping guess in valaueof
     private void checkGuess(char guess) {
+        guessedLetters += guess + " ";
+        if (!wordToGuess.contains(String.valueOf(guess))) {
+            lives --;
+        } else {
+            updateGuessedWord((guess));
+        }
     }
 
-    //show results if winning or loosing
+    //update guess word if match
+    private void updateGuessedWord(char guess) {
+    }
+
+        //show results if winning or loosing
     private void displayGameResult() {
     }
 
+    // needed to run programme
+    public static void main(String[] argv) {
+        String[] wordGuesses = { "Butterfly" , "Jupiter", "Whale", "Moon", "Cat", "Architecture", "Diamond"};
+        Main game = new Main(wordGuesses);
+        game.playGame();
+    }
 }
